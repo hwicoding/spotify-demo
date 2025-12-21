d6d0201ddf7eb93aea34c1e26273459e8f741b15
hwicoding <dlgnl117@gmail.com>
2025-12-22 01:05:32 +0900

    251222 > 라우트 설정 및 home, search 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route } from 'react-router';
+import AppLayout from './layout/AppLayout';
+import HomePage from './Pages/HomePage/HomePage';
+import SearchPage from './Pages/SearchPage/SearchPage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<AppLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="search" element={<SearchPage />} />
+      {/* <Route path="search:keyword" element={<SearchWithKeywordPage />} />
+      <Route path="playlist/:id" element={<PlaylistDetailPage />} /> */}
+      {/* <Route path="/playlist" element={<PlaylistPage/>}/> */}
+    </Route>
+  </Routes>;
 }
 
 export default App;
