72265166b1c5a112860e495b40943ce0f032fa51
hwicoding <dlgnl117@gmail.com>
2025-12-27 00:14:07 +0900

    251227 > Navbar 만들기

---
+++ src/common/components/LoginButton.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+
+const LoginButton = () => {
+  return (
+    <Button variant="contained" color="secondary" size="large">Login</Button>
+  );
+};
+export default LoginButton; 
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -2,81 +2,85 @@ import { Box, styled, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import { NavLink, Outlet } from "react-router";
-import LibraryHead from "../common/components/LibraryHead";
-import Library from "../common/components/Library";
+import LibraryHead from "./components/LibraryHead";
+import Library from "./components/Library";
+import Navbar from "./components/Navbar";
 
 const Layout = styled("div")({
-    display: "flex",
-    height: "100vh",
-    padding: "8px",
+  display: "flex",
+  height: "100vh",
+  padding: "8px",
 });
 
 const SideBar = styled("div")(({ theme }) => ({
-    width: "331px",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    [theme.breakpoints.down("sm")]: {
-        display: "none",
-    },
+  width: "331px",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
 }));
 
 const ContentBox = styled(Box)(({ theme }) => ({
-    borderRadius: "8px",
-    backgroundColor: theme.palette.background.paper,
-    color: theme.palette.text.primary,
-    width: "100%",
-    padding: "8px",
-    marginBottom: "8px",
-    marginRight: "8px",
+  borderRadius: "8px",
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+  width: "100%",
+  padding: "8px",
+  marginBottom: "8px",
+  marginRight: "8px",
 }));
 
 const NavList = styled("ul")({
-    listStyle: "none",
-    padding: "0",
-    margin: "0",
+  listStyle: "none",
+  padding: "0",
+  margin: "0",
 });
 
 const StyledNavLink = styled(NavLink)(({ theme }) => ({
-    textDecoration: "none",
-    display: "flex",
-    alignItems: "center",
-    gap: "20px",
-    color: theme.palette.text.secondary,
-    "&:hover": {
-        color: theme.palette.text.primary,
-    },
-    "&.active": {
-        color: theme.palette.text.primary,
-    },
+  textDecoration: "none",
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  color: theme.palette.text.secondary,
+  "&:hover": {
+    color: theme.palette.text.primary,
+  },
+  "&.active": {
+    color: theme.palette.text.primary,
+  },
 }))
 
 
 const AppLayout = () => {
-    return (
-        <Layout>
-            <SideBar>
-                <ContentBox>
-                    <NavList>
-                        <StyledNavLink to="/">
-                            <HomeIcon />
-                            <Typography variant="h2" fontWeight={700}>Home</Typography>
-                        </StyledNavLink>
-                        <StyledNavLink to="/search">
-                            <SearchIcon />
-                            <Typography variant="h2" fontWeight={700}>
-                                Search
-                            </Typography>
-                        </StyledNavLink>
-                    </NavList>
-                </ContentBox>
-                <ContentBox height="100%">
-                    <LibraryHead />
-                    <Library />
-                </ContentBox>
-            </SideBar>
-            <Outlet />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <SideBar>
+        <ContentBox>
+          <NavList>
+            <StyledNavLink to="/">
+              <HomeIcon />
+              <Typography variant="h2" fontWeight={700}>Home</Typography>
+            </StyledNavLink>
+            <StyledNavLink to="/search">
+              <SearchIcon />
+              <Typography variant="h2" fontWeight={700}>
+                Search
+              </Typography>
+            </StyledNavLink>
+          </NavList>
+        </ContentBox>
+        <ContentBox height="100%">
+          <LibraryHead />
+          <Library />
+        </ContentBox>
+      </SideBar>
+      <ContentBox>
+        <Navbar />
+        <Outlet />
+      </ContentBox>
+    </Layout>
+  );
 };
 export default AppLayout;
