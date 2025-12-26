344544d37c69973781f71df63d1fab2b57804b3b
hwicoding <dlgnl117@gmail.com>
2025-12-27 02:14:46 +0900

    251227 > 카드 보여주기

--- src/Pages/HomePage/components/NewReleases.tsx
+++ src/Pages/HomePage/components/NewReleases.tsx
@@ -1,15 +1,42 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import useGetNewReleases from "../../../hooks/useGetNewReleases";
+import LoadingSpinner from "../../../common/components/LoadingSpinner/LoadingSpinner";
+import ErrorMessage from "../../../common/components/ErrorMessage";
+import Card from "../../../common/components/Card";
 
 const NewReleases = () => {
   const { data, isLoading, error } = useGetNewReleases();
-  console.log("ddd", data);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+  if (error) {
+    return <ErrorMessage errorMessage={error.message} />;
+  }
   return (
     <div>
       <Typography variant="h1" paddingTop="8px">
-        New Releases Albums
+        New Released Albums
       </Typography>
+      {data && data.albums.items.length > 0 ? (
+        <Grid container spacing={2}>
+          {data.albums.items.map((album) => (
+            <Grid size={{ xs: 6, sm: 4, md: 2 }} key={album.id}>
+              <Card
+                name={album.name}
+                artistName={album.artists
+                  .map((artist) => artist.name)
+                  .join(", ")}
+                image={album.images[0].url}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="h2">No data</Typography>
+      )}
     </div>
   );
 };
+
 export default NewReleases;
